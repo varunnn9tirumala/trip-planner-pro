77c0f2aca88b893701b12d891a1fcb6b0b91a7c3
Varun Tirumala <varuntirumala570@gmail.com>
2026-02-18 12:18:59 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  preview: {
+    allowedHosts: "all",
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
